14abe53a20a989b6bb152f358d8b780418a4eba7
sandra <sandra.ruizjimenez86@gmail.com>
2023-06-15 04:48:39 +0200

    Cambiada dirección de environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8282'
-  // apiUrl: 'https://lasquefaltabanback-production.up.railway.app'
+  apiUrl: 'https://lasquefaltabanback-production.up.railway.app'
 
 };
