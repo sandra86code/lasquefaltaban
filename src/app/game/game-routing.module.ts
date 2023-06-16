fbd2506c9ea6a3197286b5bf7c156a2625871732
sandra <sandra.ruizjimenez86@gmail.com>
2023-06-16 20:20:48 +0200

    Agregados guards a country/list

--- src/app/game/game-routing.module.ts
+++ src/app/game/game-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { GameListComponent } from './game-list/game-list.component';
+import { RolGuard } from '../role-guard.guard';
+import { LoggedInGuard } from '../loggedin-guard.guard';
 
 const routes: Routes = [
   { path: '', component: GameComponent },
-  { path: 'list', component: GameListComponent}
+  { path: 'list', component: GameListComponent, canActivate: [LoggedInGuard, RolGuard]}
 ];
 
 @NgModule({
